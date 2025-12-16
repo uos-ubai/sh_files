75c1367aee9d76cbb13a1a6a77ff56006ba49692
ki5n2 <106531096+ki5n2@users.noreply.github.com>
2025-12-16 10:03:16 +0900

    Modify SLURM job script for Jupyter
    Updated SLURM job parameters and echo statements for clarity.

--- jupyter.sh
+++ jupyter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=JOB_NAME                             
-#SBATCH --partition=gpu6           # 원하는 파티션 번호로 수정                          
-##SBATCH --gres=gpu:1              # 할당받고자 하는 GPU 수로 수정(최대 4개)   
-#SBATCH --cpus-per-task=1          # 할당받고자 하는 CPU 코어 수로 수정(최대 48코어)                     
-#SBATCH -o ./output/jupyter%j.out  # STDOUT        
-#SBATCH -e ./output/jupyter%j.err  # STDERR        
+#SBATCH --job-name=MY_JOB                  # 사용자의 작업 이름으로 변경
+#SBATCH --partition=gpu6                   # 사용할 파티션 이름으로 변경 필요 -> gpu number(gpu1 ~ gpu6)
+#SBATCH --gres=gpu:1                       # 사용할 gpu 수
+#SBATCH --cpus-per-task=1                  # 하나의 태스크가 사용할 CPU 코어 수
+#SBATCH -o ./output/jupyter%j.out          # STDOUT        
+#SBATCH -e ./output/jupyter%j.err          # STDERR        
 
-echo "start at:" `date`
-echo "node: $HOSTNAME" 
-echo "jobid: $SLURM_JOB_ID" 
+echo "start at:" `date`                    # 접속한 날짜 표기
+echo "node: $HOSTNAME"                     # 접속한 노드 번호 표기
+echo "jobid: $SLURM_JOB_ID"                # jobid 표기
 
 module load cuda/11.8.0 
 
